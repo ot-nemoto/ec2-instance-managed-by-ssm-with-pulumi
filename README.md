3d8512082d3b7273536d2f287c12b87c08bafba7
ot-nemoto <41175383+ot-nemoto@users.noreply.github.com>
2024-03-25 11:24:29 +0000

    add private subnet

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # ec2-instance-managed-by-ssm-with-pulumi
+
+-   SSM で管理するプライベートネットワークのインスタンスを作成する
+
+### 事前準備
+
+```sh
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
+```
+
+### インフラストラクチャの作成
+
+```sh
+pulumi up
+```
--- index.ts
+++ index.ts
@@ -1,13 +1,24 @@
 import * as pulumi from '@pulumi/pulumi';
 import * as aws from '@pulumi/aws';
-import * as awsx from '@pulumi/awsx';
 
 const config = new pulumi.Config();
 
-const main = new aws.ec2.Vpc('main', {
+const mainVpc = new aws.ec2.Vpc('main', {
     cidrBlock: '10.0.0.0/16',
+    enableDnsHostnames: true,
 
     tags: {
         Name: config.name,
     },
 });
+
+const privateSubnet = new aws.ec2.Subnet('private', {
+    vpcId: mainVpc.id,
+    cidrBlock: '10.0.1.0/24',
+
+    tags: {
+        Name: config.name,
+    },
+});
+
+export const vpc = mainVpc.id;
