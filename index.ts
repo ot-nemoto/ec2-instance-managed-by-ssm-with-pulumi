0bb955fb8692f273fb920b759eaf611a20e1db7b
ot-nemoto <41175383+ot-nemoto@users.noreply.github.com>
2024-03-26 09:17:15 +0000

    InstanceProfileの定義を追加

--- index.ts
+++ index.ts
@@ -3,6 +3,9 @@ import * as aws from '@pulumi/aws';
 
 const config = new pulumi.Config();
 
+// =============================================================================
+// Networking
+// =============================================================================
 const mainVpc = new aws.ec2.Vpc('main', {
     cidrBlock: '10.0.0.0/16',
     enableDnsHostnames: true,
@@ -21,10 +24,40 @@ const privateSubnet = new aws.ec2.Subnet('private', {
     },
 });
 
+// =============================================================================
+// Compute
+// =============================================================================
+const assumeRole = aws.iam.getPolicyDocument({
+    statements: [
+        {
+            effect: 'Allow',
+            principals: [
+                {
+                    type: 'Service',
+                    identifiers: ['ec2.amazonaws.com'],
+                },
+            ],
+            actions: ['sts:AssumeRole'],
+        },
+    ],
+});
+const role = new aws.iam.Role('role', {
+    assumeRolePolicy: assumeRole.then((assumeRole) => assumeRole.json),
+    managedPolicyArns: ['arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore'],
+
+    tags: {
+        Name: config.name,
+    },
+});
+const profile = new aws.iam.InstanceProfile('profile', {
+    role: role.name,
+});
+
 const instance = new aws.ec2.Instance('instance', {
     ami: 'ami-0eba6c58b7918d3a1',
     instanceType: 't2.micro',
     subnetId: privateSubnet.id,
+    iamInstanceProfile: profile,
 
     tags: {
         Name: config.name,
